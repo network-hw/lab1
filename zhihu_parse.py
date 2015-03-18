c282126578bf4ffe1f14d9af0be30c0ce834d960
Vani <echooatmeal@gmail.com>
2015-03-18 14:54:22 +0800

    OOlization

--- zhihu_parse.py
+++ zhihu_parse.py
@@ -3,35 +3,46 @@ import requests
 import json
 from pyquery import PyQuery as P
 
-def parse(data):
-    headers = {
-            'user-agent': 
-            "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36"
-            }
-    try:
-        res = requests.get("http://news.at.zhihu.com/api/1.2/news/before/%d"%(data), headers = headers)
+class Zhihu:
+    def __init__(self, begin_date = 20150301, end_date = 20150318):
+        self.begin_date = begin_date
+        self.end_date = end_date
+        self.url = "http://news.at.zhihu.com/api/1.2/news/before/%d"
 
-        url = filter(lambda x: x['title'] == u"瞎扯 · 如何正确地吐槽", json.loads(res.content)['news'])[0]['url']
-        res = requests.get(url, headers = headers)
-        html = json.loads(res.content)['body']
-    except:
-        return []
+    def parse(self, date):
+        headers = {
+                'user-agent': 
+                "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36"
+                }
+        try:
+            res = requests.get(self.url%(date), headers = headers)
 
-    d = P(html)
-    result = []
-    try:
-        for i in d('.question').items():
-            now = P(i)
-            title = now('.question-title').text()
-            content = now('.answer .content').text()
-            author = now('.answer .meta .author').text()
-            if author[-1] == u"，": author = author[:-1]
-            item = {"content": "Q: " + title + " A: " + content, "data": data, "author": author}
-            result.append(json.dumps(item))
-    except:
+            url = filter(lambda x: x['title'] == u"瞎扯 · 如何正确地吐槽", json.loads(res.content)['news'])[0]['url']
+            res = requests.get(url, headers = headers)
+            html = json.loads(res.content)['body']
+        except:
+            return []
+
+        d = P(html)
+        result = []
+        try:
+            for i in d('.question').items():
+                now = P(i)
+                title = now('.question-title').text()
+                content = now('.answer .content').text()
+                author = now('.answer .meta .author').text()
+                if author[-1] == u"，": author = author[:-1]
+                content_str = "Q: " + title + " A: " + content
+                item = {"content": content_str, "date": date, "author": author, "source": "zhihu"}
+                result.append(item)
+        except:
+            return result
+
         return result
 
-    return result
+    def run(self):
+        result = []
+        for d in range(self.begin_date, self.end_date):
+            result = result + self.parse(d)
+        return result
 
-for i in parse(20150315):
-    print i
