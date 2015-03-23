1e138a359d0625ba03acdeb4e8bd062b681fabec
Vincent Zhao <vincentzhaorz@gmail.com>
2015-03-23 16:21:33 +0800

    weibo part finished

--- weibo/weibo_crawler.py
+++ weibo/weibo_crawler.py
@@ -6,24 +6,117 @@
 # 2. extract data out
 # 3. find more feeds
 
-#import os
+import os
 import sys
-import json
+import re
+import datetime
+import shutil
+import requests
+from pyquery import PyQuery as pq
 
-from weibo_login import wblogin, check_login_status
+from weibo_login import wblogin, check_login_status, session
 from weibo_feeds import weibo_fun_feeds
+
+class WeiboFeedData:
+    def __init__(self, user, text, prev, full, like, tran, comm, date):
+        self.user = user
+        self.text = text
+        self.prev = prev
+        self.full = full
+        self.like = like
+        self.tran = tran
+        self.comm = comm
+        self.date = date
+    def show(self):
+        result = {}
+        result['date'] = datetime.date.today()
+        result['source'] = 'weibo'
+        result['content'] = self.text
+        result['image-src'] = './image/' + self.prev.split('/')[-1]
+        return result
+    def download_image(self):
+        path = './image/' + self.prev.split('/')[-1]
+        if os.path.isfile(path) == False:
+            response = requests.get(self.prev, stream=True)
+            print 'Downloading', path, '...'
+            with open(path, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+
+class WeiboCrawler:
+    def __init__(self, username, password):
+        self.feed_list = []
+        self.user_name = username
+        self.pass_word = password
+        self.curr_pace = 0
+        self.init      = 0
+    def weibo_parser_feed(self, feed_content):
+        if feed_content == None or feed_content == []:
+            print "Error: Empty Parameter"
+            exit(1)
+        print feed_content
+        d = pq(feed_content)
+        user_list = d('div.c div a.nk')
+        text_list = d('div.c div span.ctt')
+        prev_list = d('div.c div a img')
+        full_list = d('div.c div a').filter(lambda i, this: pq(this).text() == u'原图')
+        like_list = d('div.c div a').filter(lambda i, this: pq(this).text().encode('utf-8').find(u'赞'.encode('utf-8')) != -1)
+        tran_list = d('div.c div a').filter(lambda i, this: pq(this).text().encode('utf-8').find(u'转发'.encode('utf-8')) != -1)
+        comm_list = d('div.c div a').filter(lambda i, this: pq(this).text().encode('utf-8').find(u'评论'.encode('utf-8')) != -1)
+        date_list = d('div.c div span.ct')
+
+        re_like = re.compile(u'赞\[(.*)\]')
+        re_tran = re.compile(u'转发\[(.*)\]')
+        re_comm = re.compile(u'评论\[(.*)\]')
+        for i in xrange(len(user_list)):
+            user = user_list[i].text
+            text = text_list[i].text
+            prev = pq(prev_list[i]).attr['src']
+            full = pq(full_list[i]).attr['href']
+            like = re_like.search(like_list[i].text).group(1)
+            tran = re_tran.search(tran_list[i].text).group(1)
+            comm = re_comm.search(comm_list[i].text).group(1)
+            date = date_list[i].text.split(' ')[0]
+            feed = WeiboFeedData(user, text, prev, full, like, tran, comm, date)
+            # Download file
+            feed.download_image()
+            self.feed_list.append(feed)
+        return 0
+    def weibo_init_run(self):
+        for feed in weibo_fun_feeds:
+            print "Fetching ...", feed[0], feed[1]
+            feed_content = session.get(feed[0]).content
+            self.weibo_parser_feed(feed_content)
+    def weibo_login(self):
+        print "Logging in Weibo ..."
+        login_status = wblogin(self.user_name, self.pass_word)
+        return check_login_status(login_status)
+    def run(self):
+        if self.init == 0:
+            if self.weibo_login() == 1:
+                print "Login failed"
+                return 0
+            self.weibo_init_run()
+            self.init = 1
+        start = self.curr_pace
+        end = self.curr_pace + 10
+        if start >= len(self.feed_list):
+            return []
+        if end >= len(self.feed_list):
+            end = len(self.feed_list)
+        self.curr_pace = end
+
+        result = []
+        for feed in self.feed_list[start:end]:
+            result.append(feed.show())
+        return result
 
 
 if __name__ == '__main__':
     if len(sys.argv[1:]) < 2:
         print "Usage: %s [username] [password]" % sys.argv[0]
         exit(1)
-    print "Logging in Weibo ..."
-    login_status = wblogin(sys.argv[1], sys.argv[2])
-    if check_login_status(login_status):
-        print "Login Succeed!"
-    else:
-        print "Login Failed"
-        exit(1)
 
-
+    crawler = WeiboCrawler(sys.argv[1], sys.argv[2])
+    for i in xrange(20):
+        print 'result: ', crawler.run()
--- weibo/weibo_feeds.py
+++ weibo/weibo_feeds.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
 weibo_fun_feeds = [
-    ('http://weibo.com/topgirls8', '微博搞笑排行榜')
+    ('http://weibo.cn/pub/category?cat=1899&page=1','搞笑分类1'),
+    ('http://weibo.cn/pub/category?cat=1899&page=2','搞笑分类2'),
+    ('http://weibo.cn/pub/category?cat=1899&page=3','搞笑分类3'),
+    ('http://weibo.cn/pub/category?cat=1899&page=4','搞笑分类4'),
+    ('http://weibo.cn/pub/category?cat=1899&page=5','搞笑分类5'),
+    ('http://weibo.cn/pub/category?cat=1899&page=6','搞笑分类6'),
+    ('http://weibo.cn/pub/category?cat=1899&page=7','搞笑分类7'),
+    ('http://weibo.cn/pub/category?cat=1899&page=8','搞笑分类8'),
+    ('http://weibo.cn/pub/category?cat=1899&page=9','搞笑分类9'),
+    ('http://weibo.cn/pub/category?cat=1899&page=10','搞笑分类10')
 ]
