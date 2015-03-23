77f88941f93baf916eb94d4db89dcb556c1c1e66
Vani <echooatmeal@gmail.com>
2015-03-23 14:14:43 +0800

    add scroll refresh

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  // Initialize template
+  var genTextItem = Handlebars.compile($('#text-item-template').html());
+  var genImageItem = Handlebars.compile($('#image-item-template').html());
+
   var container = '#content', $container = $(container), $window = $(window), $document = $(document);
 
   var wookmark = new Wookmark(container, {
@@ -7,23 +11,50 @@ $(document).ready(function() {
     verticalOffset:50
   });
 
+  var loading = false;
+
+  var dummyItem = {
+    content: "在半个世纪的政治生涯里，李光耀带领新加坡在30年内发展成为亚洲最富裕繁荣的国家之一，被誉为“新加坡国父”，是20世纪最成功的政治家之一。而在对华关系上，他既是中国人民的老朋友，一些时候又令我们心情复杂。", 
+    source: "Baidu",
+    logo: "zhihu.png"
+  };
+
+  function retrieveItems(callback) {
+    /*
+    $.get("gimme/a/hug", function(items) {
+      var newItems = [];
+      for (var i = 0; i < items.length; i++) {
+      }
+    });
+    */
+    var newItems = [];
+    for (var i = 0; i < 10; i++) {
+      newItems[i] = genTextItem(dummyItem);
+    }
+    setTimeout(function(){callback(newItems);}, 2000);
+  };
+
   function onScroll() {
     // Check if we're within 100 pixels of the bottom edge of the broser window.
     var winHeight = window.innerHeight ? window.innerHeight : $window.height(), // iphone fix
     closeToBottom = ($window.scrollTop() + winHeight > $document.height() - 100);
 
-    // dummy logic, endless scroll
-    if (closeToBottom) {
-      // Get the first then items from the grid, clone them, and add them to the bottom of the grid
-      var $items = $('.item', $container),
-      $firstTen = $items.slice(0, 10).clone().css('opacity', 0);
-      $container.append($firstTen);
-      wookmark.initItems();
-      wookmark.layout(true, function () {
-        // Fade in items after layout
-        setTimeout(function() {
-          $firstTen.css('opacity', 1);
-        }, 300);
+    if (closeToBottom && !loading) {
+      loading = true;
+      $('#loading').fadeIn(500);
+      retrieveItems(function(newItems) {
+
+        $container.append(newItems);
+        var newItemsRef = $('.new-item', $container);
+        wookmark.initItems();
+
+        wookmark.layout(true, function () {
+          loading = false;
+          $('#loading').fadeOut(300);
+          $(newItemsRef).animate({'opacity': 1}, 1000, function() {
+            $(newItemsRef).removeClass('new-item');
+          });
+        });
       });
     }
   };
